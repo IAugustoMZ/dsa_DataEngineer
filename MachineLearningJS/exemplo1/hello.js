fba1b3876621c1b125344e910ccc17a5bd71608a
IAugustoMZ <i.augustomz@gmail.com>
2024-03-17 22:40:14 -0300

    ✨feature: adds the examples of JavaScript language

---
+++ MachineLearningJS/exemplo1/hello.js
@@ -0,0 +1,13 @@
+/* Hello Worl in JavaScript */
+
+// list of strings
+var list = ["Hello", "World!", "I'm", "learning", "JavaScript"];
+
+// create a scalar
+var i = 0;
+
+// loop the list
+while (i < list.length) {
+    console.log(list[i]);
+    i++;
+}
