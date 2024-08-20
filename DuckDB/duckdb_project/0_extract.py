1e1eae5ea3e5d214ddf13b1187708b42b0f10051
Icaro Zelioli <i.augustomz@gmail.com>
2024-08-20 00:14:05 -0300

    build:implements the etl process for duckdb project

--- DuckDB/duckdb_project/0_extract.py
+++ DuckDB/duckdb_project/0_extract.py
@@ -1,14 +1,20 @@
 import os
-import duckdb
-import pandas as pd
+import warnings
 from dotenv import load_dotenv
 from services.logger import Logger
+from services.etl_services import ETLServiceHandler
+
+# ignore warnings
+warnings.filterwarnings("ignore")
 
 # load environment variables
 load_dotenv()
 
 # create logger
 logger = Logger()
+
+# create etl service handler
+etl = ETLServiceHandler()
 
 # get datasources and transaction data path
 logger.log("Getting data sources and transaction data path")
@@ -22,12 +28,25 @@ if not transaction_data_path or not quality_transactions:
 # load all dataframes
 logger.log("Loading data from sources")
 try:
-    internal_ncs = pd.read_excel(transaction_data_path,
-                                sheet_name=os.environ.get('INTERNAL_DATASOURCE'))
-    external_ncs = pd.read_excel(transaction_data_path,
-                                sheet_name=os.environ.get('EXTERNAL_DATASOURCE'))
-    cost_product = pd.read_excel(transaction_data_path,
-                                sheet_name=os.environ.get('COST_DATASOURCE'))
+    internal_ncs = etl.load_data(transaction_data_path,
+                                 sheet_name=os.environ.get('INTERNAL_DATASOURCE'))
+    external_ncs = etl.load_data(transaction_data_path,
+                                 sheet_name=os.environ.get('EXTERNAL_DATASOURCE'))
+    cost_product = etl.load_data(transaction_data_path,
+                                 sheet_name=os.environ.get('COST_DATASOURCE'))
+    
+    # setting up transaction database
+    logger.log("Setting up transaction database")
+    etl.setup_db('transactions')
+
+    # creating tables
+    logger.log("Creating tables")
+    etl.create_table(internal_ncs, 'internal_ncs')
+    etl.create_table(external_ncs, 'external_ncs')
+    etl.create_table(cost_product, 'cost_product')
+
+    # end of ETL process
+    logger.log("ETL process completed")
 except Exception as e:
     logger.error(f"Error loading data from sources: {str(e)}")
     exit()
